660a2bb3df14d673382ec733cce2b4f74ba7ad24
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-10-20 11:21:44 +0000

    10月20日 - 20:20 作成開始

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,19 @@
 class Rectangle:
-
-
-
-
+    
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+    
+    def set_width(self, w):
+        self.width = w
+    
+    def set_height(self, h):
+        self.height = h
+    
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return self.width * 2 + self.height * 2
+    
 class Square:
