92ee87c6a6c30fc830633df17b07b0ce787f6415
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-10-21 01:08:39 +0000

    10月21日 - 10:00

--- shape_calculator.py
+++ shape_calculator.py
@@ -16,4 +16,25 @@ class Rectangle:
     def get_perimeter(self):
         return self.width * 2 + self.height * 2
     
-class Square:
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+    
+    def get_picture(self):
+        
+        # Error Check
+        if self.height > 50 or self.width > 50:
+          return 'Too big for picture.'
+        
+        # Draw Rect
+        return_str = ''
+        for h in range(self.height):
+            for w in range(self.width):
+                return_str += '*'
+            return_str += '\n'
+        
+        # Return Value
+        return return_str
+        
+    def get_amount_inside(self, rect):
+        //
+#class Square:
